ba1a0efdb192e08a4f2662287ecfef03d6ec7e24
ShihKaiHo <xxxxx112358xxxxx@gmail.com>
2017-06-17 23:58:37 +0800

    new

--- Finding SAD Code/Finding_SAD_Code.cpp
+++ Finding SAD Code/Finding_SAD_Code.cpp
@@ -1,30 +1,90 @@
 #include <iostream>
 #include <stack>
+#include <vector>
 using namespace std;
 
-stack<int> code;
+struct up_down
+{
+    int up;
+    int down;
+};
+
 
 int main()
 {
+    ios::sync_with_stdio(false);
     int n;
     cin>>n;
-    int buffer;
-    bool is_CODE=0;
+    bool is_CODE=false;
+    bool stop=false;
+    int curren_min;
+    vector<up_down> range;
 
+    int buffer;
     for(int i=0;i<n;i++)
     {
         cin>>buffer;
+        curren_min=buffer;
         while(buffer!=-1)
         {
-            code.push(buffer);
+            if(stop)
+            {
+                cin>>buffer;
+                continue;
+            }
+            if(buffer>curren_min)
+            {
+                for(auto it=range.rbegin();it!=range.rend();it++)
+                {
+                    if( buffer < (it->up) && buffer > (it->down) )
+                    {
+                        is_CODE=true;
+                        stop=true;
+                        break;
+                    }
+                }
+            }
+
+            if( buffer < curren_min )
+            {
+                curren_min = buffer;
+            }
+            else if( buffer > curren_min )
+            {
+                if(!range.empty())
+                {
+                    if(curren_min == (range.back()).down)
+                    {
+                        (range.back()).up=buffer;
+                    }
+                    else
+                    {
+                        up_down temp;
+                        temp.up=buffer;
+                        temp.down=curren_min;
+                        range.push_back(temp);
+                    }
+                }
+                else
+                {
+                    up_down temp;
+                    temp.up=buffer;
+                    temp.down=curren_min;
+                    range.push_back(temp);
+                }
+
+            }
             cin>>buffer;
         }
-    }
-    while(!code.empty())
-    {
-        cout<<code.top()<<' ';
-        code.pop();
+        if(is_CODE)
+            cout<<"1\n";
+        else
+            cout<<"0\n";
+        is_CODE=0;
+        stop=0;
+        range.clear();
     }
 
 
+    return 0;
 }
