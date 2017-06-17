ed5b0744c247ea4555900c84ddb90425bba94526
ShihKaiHo <xxxxx112358xxxxx@gmail.com>
2017-06-17 23:51:04 +0800

    new

--- Finding SAD Code/Finding_SAD_Code.cpp
+++ Finding SAD Code/Finding_SAD_Code.cpp
@@ -1,30 +1,67 @@
 #include <iostream>
 #include <stack>
+#include <vector>
 using namespace std;
 
-stack<int> code;
+void print_stack(vector<int> code)
+{
+    for(auto it=code.begin();it!=code.end();it++)
+        cout<<*it<<' ';
+    cout<<endl;
+    return;
+}
 
 int main()
 {
     int n;
     cin>>n;
     int buffer;
-    bool is_CODE=0;
-
+    bool is_CODE;
+    vector<int> code;
     for(int i=0;i<n;i++)
     {
+        is_CODE=0;
         cin>>buffer;
-        while(buffer!=-1)
+        if(buffer==(-1))
         {
-            code.push(buffer);
+            cout<<"0\n";
+            continue;
+        }
+        code.push_back(buffer);
+        cin>>buffer;
+        while(buffer!=(-1))
+        {
+            print_stack(code);
+            if(buffer>code.back())
+                code.push_back(buffer);
+            else if(buffer==code.back())
+            {
+                cin>>buffer;
+                continue;
+            }
+            else
+            {
+                while(!code.empty())
+                {
+                    print_stack(code);
+                    code.pop_back();
+                    if(code.empty())
+                        break;
+                    if(buffer>code.back())
+                    {
+                        is_CODE=1;
+                        break;
+                    }
+                }
+                code.push_back(buffer);
+            }
             cin>>buffer;
         }
-    }
-    while(!code.empty())
-    {
-        cout<<code.top()<<' ';
-        code.pop();
+        print_stack(code);
+        cout<<is_CODE<<'\n';
+        code.clear();
     }
 
 
+    return 0;
 }
