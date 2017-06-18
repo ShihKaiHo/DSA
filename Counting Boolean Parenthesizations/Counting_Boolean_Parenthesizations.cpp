b1c8bf73339bed4bff9327952aa02ca0d45f8f4b
SK <xxxxx112358xxxxx@gmail.com>
2017-06-18 14:31:11 +0800

    new

--- Counting Boolean Parenthesizations/Counting_Boolean_Parenthesizations.cpp
+++ Counting Boolean Parenthesizations/Counting_Boolean_Parenthesizations.cpp
@@ -2,10 +2,66 @@
 
 using namespace std;
 
-string sym;
+string TF;
 string op;
-char temp;
+string temp;
 
+int count_TF(string& TF,string& op)
+{
+    int n=TF.length();
+    int** T=new int*[n];
+    for(int i=0;i<n;i++)
+        T[i]=new int[n];
+
+    int** F=new int*[n];
+    for(int i=0;i<n;i++)
+        F[i]=new int[n];
+
+    for(int i=0;i<n;i++)
+    {
+        T[i][i]=(TF[i] == 't')? 1: 0;
+        F[i][i]=(TF[i] == 'f')? 1: 0;
+    }
+    for(int step=1;step<n;step++)
+    {
+        for(int i=0,j=step;j<n;i++,j++)
+        {
+            T[i][j] = F[i][j] = 0;
+            for (int s=0;s<step;s++)
+            {
+                int k = i + s;
+
+                int sum_ik = T[i][k] + F[i][k];
+                int sum_kj = T[k+1][j] + F[k+1][j];
+
+
+                if (op[k] == 'a')
+                {
+                    T[i][j] += T[i][k]*T[k+1][j];
+                    F[i][j] += (sum_ik*sum_kj - T[i][k]*T[k+1][j]);
+                }
+                else if (op[k] == 'o')
+                {
+                    F[i][j] += F[i][k]*F[k+1][j];
+                    T[i][j] += (sum_ik*sum_kj - F[i][k]*F[k+1][j]);
+                }
+                else if (op[k] == 'x')
+                {
+                    T[i][j] += F[i][k]*T[k+1][j] + T[i][k]*F[k+1][j];
+                    F[i][j] += T[i][k]*T[k+1][j] + F[i][k]*F[k+1][j];
+                }
+            }
+        }
+    }
+    int buff=T[0][n-1];
+    for(int i=0;i<n;i++)
+        delete [] T[i];
+    delete [] T;
+    for(int i=0;i<n;i++)
+        delete [] F[i];
+    delete [] F;
+    return buff;
+}
 int main()
 {
     ios_base::sync_with_stdio(false);
@@ -14,15 +70,18 @@ int main()
     for(int i=0;i<n;i++)
     {
         cin>>m;
-        sym.push_back()
-        for(int j=0;j<m;j++)
+        cin>>temp;
+        TF.push_back(temp.front());
+        for(int j=0;j<m/2;j++)
         {
-
+            cin>>temp;
+            op.push_back(temp.front());
+            cin>>temp;
+            TF.push_back(temp.front());
         }
-
+        cout<<count_TF(TF,op)<<'\n';
+        TF.clear();
+        op.clear();
     }
-
-
-
     return 0;
 }
---
+++ Tobbe the half-witted Wizard of CSIE/Tobbe_the_half-witted_Wizard_of_CSIE.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+#include <stack>
+#include <string>
+#include <vector>
+#include <cstdio>
+#include <cstring>
+using namespace std;
+
+bool is_op(string);
+int opWeight(string);
+bool compare(string, string);
+vector<string> postfix;
+void to_postfix(vector<string>& infix)
+{
+    stack<string> S;
+    for(int i = 0;i< infix.size();i++)
+    {
+        if(is_op(infix[i]))
+        {
+			while(!S.empty() && S.top() != "(" && compare(S.top(),infix[i]))
+			{
+				postfix.push_back(S.top());
+				S.pop();
+			}
+			S.push(infix[i]);
+        }
+		else if(infix[i] == "("  )
+			S.push("(");
+		else if(infix[i][0] == ')')
+		{
+			while(!S.empty() && S.top() !=  "(")
+            {
+				postfix.push_back(S.top());
+				S.pop();
+			}
+			S.pop();
+		}
+		else
+        {
+            postfix.push_back(infix[i]);
+        }
+
+    }
+	while(!S.empty())
+    {
+		postfix.push_back(S.top());
+		S.pop();
+	}
+}
+
+bool is_op(string op)
+{
+	if(op == "+" || op == "-" || op == "*" || op == "/")
+		return true;
+	return false;
+}
+
+int opWeight(string op)
+{
+	if(op=="+" || op=="-")
+        return 1;
+    else
+        return 2;
+}
+
+bool compare(string op1, string op2)
+{
+	int op1Weight = opWeight(op1);
+	int op2Weight = opWeight(op2);
+
+	return op1Weight >= op2Weight ?  true: false;
+}
+
+int main()
+{
+    int n;
+    scanf("%d ",&n);
+    vector<string> infix;
+    char temp[100];
+    string buffer;
+    for(int i=0;i<n;i++)
+    {
+        gets(temp);
+        char * pch;
+        pch = strtok (temp," ");
+        while (pch != NULL)
+        {
+            infix.push_back(pch);
+            pch = strtok (NULL, " ");
+        }
+        for(auto it=infix.begin();it!=infix.end();it++)
+        {
+            if(it!=infix.end()-1)
+                printf("%s ",it->c_str());
+            else
+                printf("%s",it->c_str());
+        }
+        printf("\n");
+        to_postfix(infix);
+        for(auto it=postfix.begin();it!=postfix.end();it++)
+        {
+            if(it!=postfix.end()-1)
+                printf("%s ",it->c_str());
+            else
+                printf("%s",it->c_str());
+        }
+        printf("\n");
+        infix.clear();
+        postfix.clear();
+    }
+    return 0;
+}
