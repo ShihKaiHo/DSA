b9f7a62a237b184516dc75f6a947f510e32f9049
ShihKaiHo <xxxxx112358xxxxx@gmail.com>
2017-06-21 09:25:19 +0800

    new

---
+++ Trees transformation/Trees_transformation.cpp
@@ -0,0 +1,151 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <cstdio>
+using namespace std;
+vector<int> tree;
+int H;
+void print_preorder(int n)
+{
+    printf("%d ",tree[n]);
+    if( n > ((1<<H-1)-1) )
+    {
+        return;
+    }
+    else if(tree[2*n] == -1 && tree[2*n+1]== -1 )
+    {
+        return;
+    }
+    else if(tree[2*n+1]== -1)
+    {
+        print_preorder(2*n);
+        return;
+    }
+    else if(tree[2*n]== -1)
+    {
+        print_preorder(2*n+1);
+        return;
+    }
+    else
+    {
+        print_preorder(2*n);
+        print_preorder(2*n+1);
+        return;
+    }
+    return;
+}
+
+void print_inorder(int n)
+{
+    if( n > ((1<<H-1)-1) )
+    {
+        printf(" %d",tree[n]);
+        return;
+    }
+    if(tree[2*n] == -1 && tree[2*n+1]== -1 )
+    {
+        printf(" %d",tree[n]);
+        return;
+    }
+    else if(tree[2*n+1] == -1)
+    {
+        print_inorder(2*n);
+        printf(" %d",tree[n]);
+        return;
+    }
+    else if(tree[2*n] == -1)
+    {
+        printf(" %d",tree[n]);
+        print_inorder(2*n+1);
+        return;
+    }
+    else
+    {
+        print_inorder(2*n);
+        printf(" %d",tree[n]);
+        print_inorder(2*n+1);
+        return;
+    }
+    return;
+}
+
+void print_level_order()
+{
+    queue<int> level_order;
+    level_order.push(1);
+    int pos=1;
+    while(!level_order.empty())
+    {
+        pos=level_order.front();
+        printf("%d ",tree[pos]);
+        level_order.pop();
+        if( pos > ((1<<H-1)-1) )
+        {
+            continue;
+        }
+        else if(tree[2*pos+1] == -1 )
+        {
+            if( tree[2*pos]!= -1)
+                level_order.push(2*pos);
+        }
+        else
+        {
+            while(tree[2*pos+1] != -1)
+            {
+                if(tree[2*pos] != -1)
+                {
+                    level_order.push(2*pos);
+                }
+                pos=2*pos+1;
+                printf("%d ",tree[pos]);
+                if(pos > ((1<<H-1)-1) )
+                {
+                    break;
+                }
+            }
+            if(pos <= ((1<<H-1)-1) )
+            {
+                if(tree[2*pos]!= -1)
+                {
+                    level_order.push(2*pos);
+                }
+            }
+        }
+
+    }
+
+    return;
+}
+
+
+
+int main()
+{
+    int buffer;
+    scanf("%d",&H);
+    while(H!=0)
+    {
+        tree.push_back(0);
+        for(int i=0; i < ((1<<H) -1) ;i++)
+        {
+            scanf("%d",&buffer);
+            tree.push_back(buffer);
+        }
+        buffer=1;
+        printf("preorder: ");
+        print_preorder(buffer);
+        printf("\npostorder:");
+        buffer=1;
+        print_inorder(buffer);
+        printf("\nlevel order: ");
+        print_level_order();
+        printf("\n");
+        tree.clear();
+        scanf("%d",&H);
+        if(H!=0)
+            printf("\n");
+    }
+
+
+    return 0;
+}
